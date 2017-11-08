ccae436438fc77b44564fcbfef49b4a1afd283d0
Dengun MarcoJ <mfgjusto@gmail.com>
2017-11-08 19:00:07 +0000

    Continuing course, network lecture, changing states

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,25 @@
-import { AppRegistry } from 'react-native';
+/*import { AppRegistry } from 'react-native';
 import App from './App';
 
+AppRegistry.registerComponent('albums', () => App);*/
+
+//Index for iOS
+
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
+};
+
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
