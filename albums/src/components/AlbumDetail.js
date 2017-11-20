d5d80082b363f82ee11cfd67a8d8f416d47c8de8
Dengun MarcoJ <mfgjusto@gmail.com>
2017-11-20 19:16:58 +0000

    Finished albums and started new auth project

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, thumbnailContainerStyle, headerContentStyle, headerTextStyle } = styles;
+  const {
+        title,
+        artist,
+        thumbnail_image,
+        image,
+        url
+       } = album;
+  const {
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        headerContentStyle,
+        headerTextStyle,
+        imageStyle
+      } = styles;
   return (
     <Card>
       <CardSection>
@@ -19,6 +32,17 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}> {title}</Text>
           <Text> {artist}</Text>
         </View>
+      </CardSection>
+
+      <CardSection>
+          <Image
+            style={imageStyle}
+            source={{ uri: image }}
+          />
+      </CardSection>
+
+      <CardSection>
+        <Button text='Buy Now' onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
@@ -41,7 +65,12 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
-export default AlbumDetail
+export default AlbumDetail;
